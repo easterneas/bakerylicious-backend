281cb8ffde379f67b969220fa4ff65e5650326f7
Eas <me@eas.web.id>
2021-05-15 18:52:52 +0700

    add: getOneProduct route

--- routes/v1/products.js
+++ routes/v1/products.js
@@ -2,6 +2,7 @@ const ProductController = require('../../controllers/ProductController')
 
 module.exports = (fastify, opts, done) => {
   fastify.get('/', ProductController.getProducts)
+  fastify.get('/:ProductId', ProductController.getOneProduct)
 
   done()
 }
