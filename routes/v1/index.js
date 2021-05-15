91110df7fbe1bb64b951eb093aae6445aeb7f93a
Eas <me@eas.web.id>
2021-05-15 18:51:08 +0700

    add: customer routes

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,5 +1,6 @@
 module.exports = (fastify, opts, done) => {
   fastify.register(require('./categories'), { prefix: '/categories' })
+  fastify.register(require('./customers'), { prefix: '/customers' })
   fastify.register(require('./products'), { prefix: '/products' })
   fastify.register(require('./transactions'), { prefix: '/transactions' })
 
